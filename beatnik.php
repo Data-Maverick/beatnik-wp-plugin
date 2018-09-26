ec804c58df82a28330d94e0368a273b102499f54
Jason Norwood-Young <jason@10layer.com>
2018-09-26 08:39:38 +0200

    Include external view and style sheets; bump version number;

--- beatnik.php
+++ beatnik.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Beatnik
- * @version 0.1
+ * @version 0.1.2
  */
 /*
 Plugin Name: Beatnik
 Plugin URI: https://www.dailymaverick.co.za
 Description: Publishes Beatnik articles on the Daily Maverick CMS
 Author: Jason Norwood-Young
-Version: 0.1
+Version: 0.1.2
 Author Email: jason@10layer.com
 */
 
@@ -242,45 +242,16 @@ class beatnikArticleType {
 		}
 
 		wp_enqueue_script('beatnik-promo-script', plugin_dir_url(__FILE__) . 'js/beatnik-promo.js', array('jquery'), null, true);
+		wp_enqueue_style( 'beatnikAdminStyleSheet' );
 		$upload_link = esc_url( get_upload_iframe_src( 'image', $post->ID ) );
+		print "<div class='beatnik'>";
 		for ($x = 0; $x < self::$beatnik_tile_count; $x++) {
 			$sponsor = get_beatnik_image($post->ID, "sponsor", $x);
 			$background = get_beatnik_image($post->ID, "background", $x);
 			$blurb = get_beatnik_image($post->ID, "blurb", $x);
-			?>
-			<div class="beatnik_tile" id="beatnik_tile_<?= $x ?>_sponsor" data-tile_id="<?= $x ?>" data-tile_target="sponsor">
-				<h4>Tile <?= $x + 1 ?></h4>
-			    <a id="upload_beatnik_sponsor_logo_<?= $x ?>" class="upload-beatnik_image upload-beatnik_sponsor_logo <?= ( $sponsor["is_set"]  ) ? 'hidden' : '' ?>" href="<?php echo $upload_link ?>">Set Sponsor Logo</a>
-			    <a id="delete_beatnik_sponsor_logo_<?= $x ?>" class="delete-beatnik_image delete-beatnik_sponsor_logo <?= ( $sponsor["is_set"]  ) ? '' : 'hidden' ?> href="#">Remove Sponsor Logo</a>
-			    <input class="beatnik_sponsor_img_id" name="beatnik_sponsor_img_<?= $x ?>" type="hidden" value="<?= $sponsor[img_id] ?>" />
-				<div class="beatnik_sponsor_img_container"><?php
-				if ($sponsor["is_set"]) { ?>
-					<img src="<?= $sponsor["img_src"][0] ?>" />
-				<?php }
-				?></div>
-			</div>
-			<div class="beatnik_tile" id="beatnik_tile_<?= $x ?>" data-tile_id="<?= $x ?>_background"  data-tile_target="background">
-				<a id="upload_beatnik_background_img_<?= $x ?>" class="upload-beatnik_image upload-beatnik_background_img <?= ( $background["is_set"]  ) ? 'hidden' : '' ?>" href="<?php echo $upload_link ?>">Set Background</a>
-			    <a id="delete_beatnik_background_img_<?= $x ?>" class="delete-beatnik_image delete-beatnik_background_img <?= ( $background["is_set"]  ) ? '' : 'hidden' ?>" href="#">Remove Background</a>
-			    <input class="beatnik_background_img_id" name="beatnik_background_img_<?= $x ?>" type="hidden" value="<?= $background[img_id] ?>" />
-				<div class="beatnik_background_img_container"><?php
-				if ($background["is_set"]) { ?>
-					<img src="<?= $background["img_src"][0] ?>" />
-				<?php }
-				?></div>
-			</div>
-			<div class="beatnik_tile" id="beatnik_tile_<?= $x ?>" data-tile_id="<?= $x ?>_blurb"  data-tile_target="blurb">
-				<a id="upload_beatnik_blurb_img_<?= $x ?>" class="upload-beatnik_image upload-beatnik_blurb_img <?= ( $blurb["is_set"]  ) ? 'hidden' : '' ?>" href="<?php echo $upload_link ?>">Set Blurb</a>
-			    <a id="delete_beatnik_blurb_img_<?= $x ?>" class="delete-beatnik_image delete-beatnik_blurb_img <?= ( $blurb["is_set"]  ) ? '' : 'hidden' ?>" href="#">Remove Blurb</a>
-			    <input class="beatnik_blurb_img_id" name="beatnik_blurb_img_<?= $x ?>" type="hidden" value="<?= $blurb[img_id] ?>" />
-				<div class="beatnik_blurb_img_container"><?php
-				if ($blurb["is_set"]) { ?>
-					<img src="<?= $blurb["img_src"][0] ?>" />
-				<?php }
-				?></div>
-			</div>
-			<?php
+			include(plugin_dir_path(__FILE__) . "views/admin/tile.php");
 		}
+		print "</div>";
 	}
 
 	public function beatnik_blurb_box_html() {
@@ -365,6 +336,7 @@ function beatnik_logo_url() {
 }
 
 wp_register_style( 'beatnikStyleSheet', plugins_url( 'css/beatnik.css', __FILE__ ) );
+wp_register_style( 'beatnikAdminStyleSheet', plugins_url( 'css/beatnik-admin.css', __FILE__ ) );
 
 define("THREECOLUMN", 0);
 define("WIDE", 1);
